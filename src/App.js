01b449f492a32d4728c9a7e74e97a7518cc72693
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2023-09-26 16:12:20 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Soundbar from "./components/Soundbar"; 
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
@@ -17,7 +16,6 @@ function App() {
       <About />
       <Skills />
       <Works />
-      <Contact />
     </div>
   );
 }
