43d4dd34adee1244c7cc3e15b4e30c08aad5dd07
jaydess21 <98706895+jaydess21@users.noreply.github.com>
2023-10-01 19:32:46 -0400

    Update change
    change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
           <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8 mb-1">
             <div className="sm:text-right pb-8 pl-4">
               <p className="text-3xl font-bold inline border-b-4 border-pink-500">
-                About
+                About Jayde
               </p>
             </div>
             <div></div>
